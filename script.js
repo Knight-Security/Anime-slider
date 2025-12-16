58852af5afeeeb8139126a3d02d8683a54a7aa7a
knight-Security <paliwaladitya24@gmail.com>
2025-12-17 00:44:06 +0530

    added rating and no of episodes

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const pictures = Array.from(document.getElementsByClassName("pic"));
 const backwardButton = document.getElementById("backward");
 const forwardButton = document.getElementById("forward");
-const animeName=document.getElementById("text");
+const animeName = document.getElementById("text");
+const episodes = document.getElementById("noofEpisodes");
+const rating = document.getElementById("anime-rating");
 // all anime names
-const Name=["The Misfit of Demon King Academy","Demon Slayer","Jujutsu Kaisen","Lord of Mysteries","One Punch Man","Mashle: Magic and Muscles","Fullmetal Alchemist","How a Realist Hero Rebuilt the Kingdom"];
+const Name = ["The Misfit of Demon King Academy", "Demon Slayer", "Jujutsu Kaisen", "Lord of Mysteries", "One Punch Man", "Mashle: Magic and Muscles", "Fullmetal Alchemist", "How a Realist Hero Rebuilt the Kingdom"];
 // all anime summary
-const animeSummary=[];
+const animeSummary = [];
 // all anime rating 
-const animeRating=[];
+const animeRating = [7.5, 9, 9, 9.5, 8, 9.8, 8, 7.5];
 // all anime No of Episodes
-const animeEpisodes=[];
+const animeEpisodes = [24, 93, 47, 15, 34, 24, 64, 26];
 let counter = 0;
 pictures.forEach((picture, index) => {
   picture.style.left = `${index * 100}%`;
@@ -24,20 +26,28 @@ const slideImage = () => {
 
 forwardButton.addEventListener("click", () => {
   counter++;
-  animeName.innerText=Name[counter];
+  animeName.innerText = Name[counter];
+  episodes.innerText = animeEpisodes[counter];
+  rating.innerText = animeRating[counter];
+
   if (counter >= pictures.length) {
     counter = 0;
-    animeName.innerText=Name[counter];
+
+    animeName.innerText = Name[counter];
+    episodes.innerText = animeEpisodes[counter];
+    rating.innerText = animeRating[counter];
   }
-  
+
   slideImage();
 });
 backwardButton.addEventListener("click", () => {
   counter--;
-  animeName.innerText=Name[counter];
+  animeName.innerText = Name[counter];
   if (counter < 0) {
     counter = pictures.length - 1;
-    animeName.innerText=Name[counter];
+    animeName.innerText = Name[counter];
+    episodes.innerText = animeEpisodes[counter];
+    rating.innerText = animeRating[counter];
   }
   slideImage();
 });
